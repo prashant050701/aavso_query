8fe57b8ad5984e4e9b0a9d39b8b53490b1cd241c
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-06-30 15:05:18 +0200

    Add files via upload

---
+++ example.py
@@ -0,0 +1,17 @@
+from query import AAVSODataFetcher
+from plotter import Plotter
+
+fetcher = AAVSODataFetcher(
+    star_name='T CrB',
+    obs_types='vis',
+    num_results=100,
+    pages=1
+)
+
+julian_dates, magnitudes = fetcher.fetch_and_parse_data(include_uncertain=False)
+
+if len(julian_dates) > 0 and len(magnitudes) > 0:
+    plotter = Plotter(julian_dates, magnitudes)
+    plotter.plot_light_curve(interval_hours=1)
+else:
+    print("No data available to plot.")
